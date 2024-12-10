28b44ae54fed97feece0623bcbfd277c0825750e
K024C1022 <K024C1022@g.neec.ac.jp>
2024-12-10 02:49:57 +0000

    Update

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-from flask import Flask, request
+from flask import Flask
+from flask import make_response,request # Cookieのため --- (*1)
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # URLパラメータを取得 --- (*1)
-    a = request.args.get('a')
-    b = request.args.get('b')
-    # パラメータが設定されているか確認 --- (*2)
-    if (a is None) or (b is None):
-        return "パラメータが足りません。"
-    # パラメータを数値に変換して計算 --- (*3)
-    c = int(a) * int(b)
-    # 結果を出力 --- (*4)
-    return "<h1>" + str(c) + "</h1>"
+    # Cookieの値を取得 --- (*2)
+    cnt_s = request.cookies.get('cnt')
+    if cnt_s is None:
+        cnt = 0
+    else:
+        cnt = int(cnt_s)
+    # 訪問回数カウンタに1加算 --- (*3)
+    cnt += 1
+    response = make_response("""
+        <h1>訪問回数: {}回</h1>
+    """.format(cnt))
+    # Cookieに値を保存 --- (*4)
+    max_age = 60 * 60 * 24 * 90 # 90日
+    expires = int(datetime.now().timestamp()) + max_age
+    response.set_cookie('cnt', value=str(cnt),
+            max_age=max_age,
+            expires=expires)
+    return response
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
